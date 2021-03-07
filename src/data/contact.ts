fb3227aeaa45d2a599a7a7d5fae5a728995470a7
Raymond Su <rayonixs@gmail.com>
2021-03-07 20:33:00 +1100

    fix: email link incorrect

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,6 +1,6 @@
 export const ContactLinks = {
   github: 'https://github.com/Raymond-Su',
   linkedin: 'https://linkedin.com/in/raymond-su/',
-  gmail: 'mailto:su.raymond@outlook.com.au',
+  gmail: 'su.raymond@outlook.com.au',
   medium: 'https://medium.com/@raymondsu'
 };
