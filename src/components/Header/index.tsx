bc6e34afc50f9536b75430ff990cecfb10909bcf
Raymond Su <rayonixs@gmail.com>
2021-03-07 20:31:57 +1100

    fix: header right align

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,32 +7,17 @@ const Header: FC = () => {
   return (
     <Headroom>
       <header className='header'>
-        <a href='/' className='logo'>
-          <span className='grey-color'> &lt;</span>
-          <span className='logo-name'>Raymond Su</span>
-          <span className='grey-color'>/&gt;</span>
-        </a>
         <input className='menu-btn' type='checkbox' id='menu-btn' />
         <label className='menu-icon' htmlFor='menu-btn' style={{ color: 'white' }}>
           <span className='navicon' />
         </label>
-        <ul className='menu'>
-          <li>
-            <a href='#Home'>About Me</a>
-          </li>
-          <li>
-            <a href='#skills'>Skills</a>
-          </li>
-          <li>
-            <a href='#experiences'>Experiences</a>
-          </li>
-          <li>
-            <a href='#projects'>Projects</a>
-          </li>
-          <li>
-            <a href='#contact'>Contact Me</a>
-          </li>
-        </ul>
+        <div className='menu'>
+          <a href='#Home'>About Me</a>
+          <a href='#skills'>Skills</a>
+          <a href='#experiences'>Experiences</a>
+          <a href='#projects'>Projects</a>
+          <a href='#contact'>Contact Me</a>
+        </div>
       </header>
     </Headroom>
   );
